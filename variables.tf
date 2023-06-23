06f62256b34711f432ea78ee57e223732d641afa
nagbabu <nagbabu2133@gmail.com>
2023-06-23 17:40:47 +0530

    eit the files

--- variables.tf
+++ variables.tf
@@ -5,7 +5,6 @@ variable "aws_region" {}
 variable "vpc_cidr" {}
 variable "vpc_name" {}
 variable "IGW_name" {}
-variable "key_name" {}
 variable "public_subnet1_cidr" {}
 variable "public_subnet2_cidr" {}
 variable "public_subnet3_cidr" {}
@@ -13,7 +12,7 @@ variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
 
-variable Main_Routing_Table {}
+variable "Main_Routing_Table" {}
 variable "azs" {}
 
 
