f43cc79c73943ae65d68f80c539d96bbaec1ae08
nagbabu <nagbabu2133@gmail.com>
2023-06-23 17:34:46 +0530

    edit all am drg

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "aws_internet_gateway" "default" {
 resource "aws_subnet" "subnet1-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet1_cidr}"
-    availability_zone = "us-east-1a"
+    availability_zone = "ap-south-1a"
 
     tags = {
         Name = "${var.public_subnet1_name}"
@@ -34,7 +34,7 @@ resource "aws_subnet" "subnet1-public" {
 resource "aws_subnet" "subnet2-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet2_cidr}"
-    availability_zone = "us-east-1b"
+    availability_zone = "ap-south-1b"
 
     tags = {
         Name = "${var.public_subnet2_name}"
@@ -44,7 +44,7 @@ resource "aws_subnet" "subnet2-public" {
 resource "aws_subnet" "subnet3-public" {
     vpc_id = "${aws_vpc.default.id}"
     cidr_block = "${var.public_subnet3_cidr}"
-    availability_zone = "us-east-1c"
+    availability_zone = "ap-southa-1c"
 
     tags = {
         Name = "${var.public_subnet3_name}"
--- variables.tf
+++ variables.tf
@@ -9,17 +9,13 @@ variable "key_name" {}
 variable "public_subnet1_cidr" {}
 variable "public_subnet2_cidr" {}
 variable "public_subnet3_cidr" {}
-variable "private_subnet_cidr" {}
 variable "public_subnet1_name" {}
 variable "public_subnet2_name" {}
 variable "public_subnet3_name" {}
-variable "private_subnet_name" {}
+
 variable Main_Routing_Table {}
-variable "azs" {
- 
-  default = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-}
-variable "environment" { default = "dev" }
+variable "azs" {}
+
 
 
 
